9d7bd847271bd3bc4c06a263cf548f77945b7da6
HabeebaMaryam <168250809+HabeebaMaryam@users.noreply.github.com>
2025-05-10 16:19:57 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# comments 
+"single line comment ---- #"
+"multi line comments -----
+  << comment
+
+  comment
+
+
+name="this is name variable" ----- to print the variable value in echo use doller $name
 
